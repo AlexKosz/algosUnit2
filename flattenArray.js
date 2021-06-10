5e2df29a6af1c0c3b9acc046afaa2be0dbef19dc
Zanzong43 <alexkoszuta43@gmail.com>
2021-06-10 11:36:11 -0400

    re-added old code as comments for easy comparison

--- flattenArray.js
+++ flattenArray.js
@@ -1,3 +1,29 @@
+//before
+// function flattenArray(arr) {
+//     //loop trhough array
+//     for (let i = 0; i < arr.length; i++) {
+//         if (Array.isArray(arr[i])) { //if array position is an array do this
+//             let flat = flattenArray(arr[i]) //call function recursivly to handle that array having an array
+//             for (let j = 0; j < flat.length; j++) { //loop through the result of the previous function call (line 5)
+//                 if (flat[0]) {                      //if there is a value in the array
+//                     arr.splice((i), 0, flat[0])     //splice it in to the array passed in at index i
+//                     flat.shift();                   //remove first value from the result of line 5 
+//                 }
+//             }
+//         }
+//         if (arr[i].length == 0) {                   //if there is an empty array at spot i
+//             arr.splice(i, 1);                       //delete that item
+//             i--;                                    //decrement i to account for decrease in length
+//         }
+//     }
+//     return arr;
+
+// }
+
+
+
+
+//After
 function flattenArray(arr) {
     //loop through array
     for (let i = 0; i < arr.length; i++) {
